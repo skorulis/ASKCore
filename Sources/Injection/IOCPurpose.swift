037e97c96037c3cc8cd77ed500d69bfd1e09f03a
Alex Skorulis <skorulis@gmail.com>
2022-07-21 05:48:07 +1000

    Add a purpose for IOC

---
+++ Sources/Injection/IOCPurpose.swift
@@ -0,0 +1,13 @@
+//  Created by Alexander Skorulis on 20/7/2022.
+
+import Foundation
+
+/// General purpose for the IOC setup
+public enum IOCPurpose {
+    
+    /// Normal app running
+    case normal
+    
+    /// Unit tests or previews
+    case testing
+}
--- Sources/Injection/IOCService.swift
+++ Sources/Injection/IOCService.swift
@@ -12,9 +12,11 @@ import SwinjectAutoregistration
 open class IOCService: PContainerFactory {
     
     public let container: Container
+    public let purpose: IOCPurpose
     
-    public init() {
+    public init(purpose: IOCPurpose) {
         container = Container()
+        self.purpose = purpose
         
         let fac = container.register(GenericFactory.self) { [unowned self] res in
             return GenericFactory(container: self.container)
