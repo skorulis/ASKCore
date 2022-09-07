ac2701c842d20f884cb3c85cc63b946e82a61db9
Alex Skorulis <skorulis@gmail.com>
2022-09-08 09:56:50 +1000

    Improve module setup

--- Sources/Injection/Module/PModule.swift
+++ Sources/Injection/Module/PModule.swift
@@ -13,12 +13,13 @@ public protocol PModule {
 
 public extension PModule {
     
-    static func ioc(purpose: IOCPurpose = .testing) -> IOCService {
+    static func ioc(purpose: IOCPurpose = .testing, adjust: (Container) -> Void = {_ in }) -> IOCService {
         let ioc = IOCService(purpose: purpose)
         let registration = ioc.resolve(ModuleRegistrationService.self)
         registration.register(container: ioc.container,
                               purpose: purpose,
                               module: self)
+        adjust(ioc.container)
         return ioc
     }
     
