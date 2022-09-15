1d51996a2a9de15ec554947cab40d1a90c4eda32
Alex Skorulis <skorulis@gmail.com>
2022-09-15 19:14:49 +1000

    Add persistence

---
+++ Sources/Persistence/InMemoryDefaults.swift
@@ -0,0 +1,27 @@
+//  Created by Alexander Skorulis on 20/7/2022.
+
+import Foundation
+
+public final class InMemoryDefaults {
+    
+    var storage: [String: Any] = [:]
+    
+    public init() {}
+    
+}
+
+extension InMemoryDefaults: PKeyValueStore {
+    public func data(forKey: String) -> Data? {
+        return storage[forKey] as? Data
+    }
+    
+    public func set(_ value: Any?, forKey: String) {
+        if let value = value {
+            storage[forKey] = value
+        } else {
+            storage.removeValue(forKey: forKey)
+        }
+    }
+    
+    
+}
