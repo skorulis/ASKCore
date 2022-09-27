95872e8a7798b1150c25d8f0af0d0e43d89a71f3
Alex Skorulis <skorulis@gmail.com>
2022-09-27 20:29:29 +1000

    Improve coordination

---
+++ Sources/Coordinator/CoordinatorView.swift
@@ -0,0 +1,47 @@
+//  Created by Alexander Skorulis on 27/9/2022.
+
+import Foundation
+import SwiftUI
+
+@available(iOS 16, *)
+public struct CoordinatorView<T: PCoordinator>: View {
+    
+    @StateObject var coordinator: T
+    @Environment(\.dismiss) private var dismiss
+    
+    public init(coordinator: T) {
+        _coordinator = StateObject(wrappedValue: coordinator)
+    }
+    
+    public var body: some View {
+        NavigationStack(path: $coordinator.navPath) {
+            coordinator.root.render(coordinator: coordinator)
+                .navigationDestination(for: T.PathType.self) { path in
+                    path.render(coordinator: coordinator)
+                }
+        }
+        .onChange(of: coordinator.shouldDismiss) { value in
+            if value {
+                dismiss()
+            }
+        }
+        .fullScreenCover(item: binding(style: .fullScreen)) { presented in
+            CoordinatorView(coordinator: presented.coordinator)
+        }
+        .sheet(item: binding(style: .sheet)) { presented in
+            CoordinatorView(coordinator: presented.coordinator)
+        }
+    }
+    
+    private func binding(style: PresentationStyle) -> Binding<PresentedCoordinator<T>?> {
+        return Binding<PresentedCoordinator<T>?> {
+            return coordinator.presented?.only(style: style)
+        } set: { newValue in
+            if newValue == nil && coordinator.presented != nil {
+                coordinator.presented = newValue
+            }
+        }
+    }
+    
+}
+
--- Sources/Coordinator/PCoordinator.swift
+++ Sources/Coordinator/PCoordinator.swift
@@ -3,16 +3,73 @@
 import Foundation
 import SwiftUI
 
+@available(iOS 16, *)
 public protocol CoordinatorPath: Hashable {
     associatedtype CoordinatorType: PCoordinator where CoordinatorType.PathType == Self
     associatedtype ViewType: View
     
     func render(coordinator: CoordinatorType) -> ViewType
+    
+    var id: String { get }
 }
 
-public protocol PCoordinator {
+@available(iOS 16, *)
+public protocol PCoordinator: ObservableObject {
     associatedtype PathType: CoordinatorPath where PathType.CoordinatorType == Self
     
-    func push(_ p: PathType)
-    func pop()
+    var navPath: NavigationPath { get set }
+    var presented: PresentedCoordinator<Self>? { get set }
+    var shouldDismiss: Bool { get set }
+    var root: PathType { get }
+    
+    /// Create a child coordinator for presentation
+    func child(path: PathType) -> Self
+    
+}
+
+@available(iOS 16, *)
+public extension PCoordinator {
+    
+    func push(_ p: PathType) {
+        navPath.append(p)
+    }
+    
+    func pop() {
+        navPath.removeLast()
+    }
+    
+    func present(_ path: PathType, style: PresentationStyle) {
+        let coordinator = child(path: path)
+        self.presented = PresentedCoordinator(coordinator: coordinator, style: style)
+    }
+    
+}
+
+@available(iOS 16, *)
+public struct PresentedCoordinator<T: PCoordinator>: Identifiable {
+    
+    public let coordinator: T
+    public let style: PresentationStyle
+    
+    public var id: String {
+        return coordinator.root.id
+    }
+    
+    public init(coordinator: T, style: PresentationStyle) {
+        self.coordinator = coordinator
+        self.style = style
+    }
+    
+    func only(style: PresentationStyle) -> Self? {
+        switch (self.style, style) {
+        case (.fullScreen, .fullScreen): return self
+        case (.sheet, .sheet): return self
+        default: return nil
+        }
+    }
+    
+}
+
+public enum PresentationStyle {
+    case fullScreen, sheet
 }
