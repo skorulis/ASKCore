137225c02231d762379c113e3cbbf4c6bfcbcdd6
Alex Skorulis <skorulis@gmail.com>
2023-04-02 19:18:55 +1000

    Extra combine helper

--- Sources/Combine/Publisher+Extensions.swift
+++ Sources/Combine/Publisher+Extensions.swift
@@ -8,4 +8,25 @@ public extension Publisher {
     func delayedChange() -> Publishers.Debounce<Self, DispatchQueue> {
         debounce(for: .zero, scheduler: .main)
     }
+    
+    
+}
+
+public extension Publisher where Failure == Error {
+    
+    // Sink version giving a single result
+    func sink(result: @escaping (Result<Self.Output, Self.Failure>) -> Void) -> AnyCancellable {
+        sink { completion in
+            switch completion {
+            case .finished:
+                break // Use value from receiveValue
+            case let .failure(error):
+                result(.failure(error))
+            }
+        } receiveValue: { value in
+            result(.success(value))
+        }
+    }
+    
+    
 }
