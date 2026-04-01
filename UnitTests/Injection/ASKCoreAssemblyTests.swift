c13e3fdeb1edf8fcd6e47e894aa98ad69f96ba19
Alex Skorulis <skorulis@gmail.com>
2026-04-01 14:09:51 +1100

    Add tests for assembly setup

---
+++ UnitTests/Injection/ASKCoreAssemblyTests.swift
@@ -0,0 +1,19 @@
+//  Created by Alex Skorulis on 1/4/2026.
+
+import Foundation
+import Knit
+import XCTest
+@testable import ASKCore
+
+final class ASKCoreAssemblyTests: XCTestCase {
+    
+    @MainActor
+    func test_resolution() {
+        let assembler = ScopedModuleAssembler<BaseResolver>([ASKCoreAssembly()])
+        
+        XCTAssertTrue(assembler.resolver.isAvailable)
+        let keyValueStore = assembler.resolver.pKeyValueStore()
+        XCTAssertTrue(keyValueStore is InMemoryDefaults)
+    }
+}
+
